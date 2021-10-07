674b0f6a66f4fc0ca7f55036963f7b25b7ff7262
Zakaria HADI <hadizakaria@yahoo.com>
2021-10-08 01:30:59 +0200

    add files config

---
+++ PART1/provision.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Paranoia mode
+set -e
+set -u
+
+HOSTNAME="$(hostname)"
+
+export DEBIAN_FRONTEND=noninteractive
+
+## Vérifier que le fichier .env est bien défini
+if [ ! -f /vagrant/.env ]; then
+	>&2 echo "ERROR: unable to find /vagrant/.env file"
+	exit 1
+fi
+
+
+# Mettre à jour le catalogue des paquets debian
+apt-get update --allow-releaseinfo-change
+
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    git \
+    curl \
+    wget \
+    vim \
+    gnupg2 \
+    net-tools \
+    bash \
+    sudo \
+    software-properties-common
+
+
+if [ "$HOSTNAME" = "s1.infra" ]; then
+	# dnsmask installation 
+    apt-get install -y dnsmasq resolvconf
+
+    # dnsmasq pass configurations
+    # dhcp-option=3  // remove the default gateway route
+    # dhcp-range=192.168.50.10,192.168.50.20
+    # dhcp-host=mac adress:19.2....
+    # confs => /etc/dnsmasq.conf
+    # sudo dhclient -r eth1 // To renew or release an IP address for the eth1 interface, enter:
+    # sudo dhclient eth1
+
+    
+    # enable dnsmasq service
+    #systemctl start dnsmasq
+
+	# apt-get install -y \
+		# puppet-master \
+    
+fi
+
+# else
+# 	# J'installe puppet dessus
+# 	apt-get install -y \
+# 		puppet
+# fi
+
+# Désactive l'update automatique du cache apt + indexation (lourd en CPU)
+cat >> /etc/apt/apt.conf.d/99periodic-disable <<MARK
+APT::Periodic::Enable "0";
+MARK
+
+
+echo "SUCCESS."
+
